0637f87de12dcf418c472c45196d5cd025f7fcbe
majd161997 <majd.han97@hotmail.com>
2020-10-12 23:13:21 +0300

    fixes

--- src/components/Root/Stats/Stats.js
+++ src/components/Root/Stats/Stats.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 class Stats extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             content_title: 'Accounts',
             table: ''
         };
@@ -14,88 +14,87 @@ class Stats extends React.Component {
 
     getAccounts() {
         fetch("http://localhost:9000/serverAPI/getAccounts",
-        {
-            method: 'post',
-        })
-        .then(response => response.json())
-        .then((res) => {
-            let accountsTable = [];
-            res.map(data => (
-                accountsTable.push(
-                    <tr>
-                        <td>{data.id}</td>
-                        <td>{data.f_name}</td>
-                        <td>{data.l_name}</td>
-                        <td>{data.email}</td>
-                        <td>{data.password}</td>
-                        <td>{data.permission}</td>
-                    </tr>
-                )
-        ))
-        this.setState({table: accountsTable});
-        })
-        .catch((error) => {
-           console.log(error);
-         });
+            {
+                method: 'post',
+            })
+            .then(response => response.json())
+            .then((res) => {
+                let accountsTable = [];
+                res.map(data => (
+                    accountsTable.push(
+                        <tr key={accountsTable}>
+                            <td>{data.id}</td>
+                            <td>{data.f_name}</td>
+                            <td>{data.l_name}</td>
+                            <td>{data.email}</td>
+                            <td>{data.password}</td>
+                            <td>{data.permission}</td>
+                        </tr>
+                    )
+                ))
+                this.setState({ table: accountsTable });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     getSearchHistory() {
         fetch("http://localhost:9000/serverAPI/getSearchHistoryTable",
-        {
-            method: 'post',
-        })
-        .then(response => response.json())
-        .then((res) => {
-            let searchTable = [];
-            res.map(data => (
-                searchTable.push(
-                    <tr>
-                        <td>{data.sid}</td>
-                        <td>{data.account_id}</td>
-                        <td>{data.search_p}</td>
-                    </tr>
-                )
-        ))
-        this.setState({table: searchTable});
-        })
-        .catch((error) => {
-           console.log(error);
-         });
+            {
+                method: 'post',
+            })
+            .then(response => response.json())
+            .then((res) => {
+                let searchTable = [];
+                res.map(data => (
+                    searchTable.push(
+                        <tr key={searchTable}>
+                            <td>{data.sid}</td>
+                            <td>{data.account_id}</td>
+                            <td>{data.search_p}</td>
+                        </tr>
+                    )
+                ))
+                this.setState({ table: searchTable });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     getViewedHistory() {
         fetch("http://localhost:9000/serverAPI/getViewedHistoryTable",
-        {
-            method: 'post',
-        })
-        .then(response => response.json())
-        .then((res) => {
-            let viewedTable = [];
-            res.map(data => (
-                viewedTable.push(
-                    <tr>
-                        <td>{data.vid}</td>
-                        <td>{data.account_id}</td>
-                        <td>{data.video_id}</td>
-                        <td>{data.imageURL}</td>
-                        <td>{data.Title}</td>
-                    </tr>
-                )
-        ))
-        this.setState({table: viewedTable});
-        })
-        .catch((error) => {
-           console.log(error);
-         });
+            {
+                method: 'post',
+            })
+            .then(response => response.json())
+            .then((res) => {
+                let viewedTable = [];
+                res.map(data => (
+                    viewedTable.push(
+                        <tr key={viewedTable}>
+                            <td>{data.vid}</td>
+                            <td>{data.account_id}</td>
+                            <td>{data.video_id}</td>
+                            <td>{data.imageURL}</td>
+                            <td>{data.Title}</td>
+                        </tr>
+                    )
+                ))
+                this.setState({ table: viewedTable });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     menuBtnHandle = (event) => {
         event.preventDefault();
-        this.setState({table: ''});
-        this.setState({content_title: event.target.value}, 
-            function(){
-                switch(this.state.content_title)
-                {
+        this.setState({ table: '' });
+        this.setState({ content_title: event.target.value },
+            function () {
+                switch (this.state.content_title) {
                     case 'Accounts':
                         this.getAccounts();
                         break;
@@ -104,42 +103,47 @@ class Stats extends React.Component {
                         break;
                     case 'Videos history':
                         this.getViewedHistory();
-                            break;
-                        default:
-                            break;
+                        break;
+                    default:
+                        break;
                 }
             });
     }
 
     render() {
         let table;
-        switch(this.state.content_title)
-        {
-            case 'Accounts':
-                table = <table className={Styles.table}>
-                        <tr>
-                            <th>ID</th>
-                            <th>First name</th>
-                            <th>Last name</th>
-                            <th>Email</th>
-                            <th>Password</th>
-                            <th>Permission</th>
-                        </tr>
-                        {this.state.table}
-                        </table>
-                break;
-            case 'Search history':
-                table = <table className={Styles.table}>
-                        <tr>
-                            <th>SID</th>
-                            <th>Account ID</th>
-                            <th>Search input</th>
-                        </tr>
-                        {this.state.table}
-                        </table>
-                break;
-            case 'Videos history':
+        if (this.state.table) {
+            switch (this.state.content_title) {
+                case 'Accounts':
                     table = <table className={Styles.table}>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>First name</th>
+                                <th>Last name</th>
+                                <th>Email</th>
+                                <th>Password</th>
+                                <th>Permission</th>
+                            </tr>
+                        </thead>
+                        <tbody>{this.state.table}</tbody>
+                    </table>
+                    break;
+                case 'Search history':
+                    table = <table className={Styles.table}>
+                        <thead>
+                            <tr>
+                                <th>SID</th>
+                                <th>Account ID</th>
+                                <th>Search input</th>
+                            </tr>
+                        </thead>
+                        <tbody>{this.state.table}</tbody>
+                    </table>
+                    break;
+                case 'Videos history':
+                    table = <table className={Styles.table}>
+                        <thead>
                             <tr>
                                 <th>VID</th>
                                 <th>Account ID</th>
@@ -147,13 +151,15 @@ class Stats extends React.Component {
                                 <th>Image URL</th>
                                 <th>Title</th>
                             </tr>
-                            {this.state.table}
-                            </table>
+                        </thead>
+                        <tbody>{this.state.table}</tbody>
+                    </table>
                     break;
                 default:
                     break;
+            }
         }
-        return(
+        return (
             <div>
                 <Link to="/home">Back to home</Link>
                 <div className={Styles.leftBar}>
@@ -162,7 +168,7 @@ class Stats extends React.Component {
                     <button onClick={this.menuBtnHandle} value="Videos history">Videos history</button>
                 </div>
                 <div className={Styles.content}>
-                    <span style={{fontWeight: 'bold'}}>{this.state.content_title}</span>
+                    <span style={{ fontWeight: 'bold' }}>{this.state.content_title}</span>
                     {table}
                 </div>
             </div>
